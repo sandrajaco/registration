9ad94b7d77a1cc0568e3cddda9832c335a75353f
sandrajaco <sandrajaco@gmail.com>
2016-11-09 01:57:17 +0530

    Add files via upload

---
+++ controllers/register.controller.js
@@ -0,0 +1,44 @@
+﻿var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('config.json');
+
+router.get('/', function (req, res) {
+   // log user out
+  //delete req.session.token;
+
+    // move success message into local variable so it only appears once (single read)
+    var viewData = { success: req.session.success };
+    delete req.session.success;
+
+    res.render('register',viewData);
+});
+
+router.post('/', function (req, res) {
+    // register using api to maintain clean separation between layers
+    request.post({
+        url: config.apiUrl + '/users/register',
+        form: req.body,
+        json: true
+    }, function (error, response, body) {
+        if (error) {
+            return res.render('register', { error: 'An error occurred' });
+        }
+
+        if (response.statusCode !== 200) {
+            return res.render('register', {
+                error: response.body,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                username: req.body.username
+            });
+        }
+
+        // return to login page with success message
+        req.session.success = 'Registration successful for '+req.body.username;
+        
+        return res.redirect('/register');
+    });
+});
+
+module.exports = router;
